ebce3fdf743e17cf5f9af19b3cf143f284792db4
SarahZhao806 <yzhao806@gmail.com>
2016-09-06 15:11:15 -0400

    Created a WebSocket server and broadcasting messages with all its clents by using ws module

---
+++ ws.js
@@ -0,0 +1,18 @@
+var WebSocketServer = require('ws').Server;
+
+//ws://
+var wss = new WebSocketServer({port: 3000});
+wss.on('connection', function(ws) {
+    
+    ws.on('message', function(message) {
+       if(message === 'exit') {
+           ws.close();
+       } else {
+           wss.clients.forEach(function(client) {
+            client.send(message);
+           });
+       }
+    });
+    ws.send('Welcome to cyber chat');
+});
+
