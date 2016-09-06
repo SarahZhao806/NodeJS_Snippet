6fb07ac25b9dc43940294644e5bbfa4f06267eb0
SarahZhao806 <yzhao806@gmail.com>
2016-09-06 16:18:22 -0400

    Mocha testing Asynchronous functions

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -2,10 +2,23 @@ var expect = require('chai').expect;
 
 var tools = require('../lib/tools');
 
-describe('printName()', function() {
-    it('should print the last name first', function() {
-        var results = tools.printName({first: 'Alex', last: 'Banks'});
-        
-        expect(results).to.equal("Banks, Alex");
+describe('Tools', function() {
+        describe('printName()', function() {
+        it('should print the last name first', function() {
+            var results = tools.printName({first: 'Alex', last: 'Banks'});
+            expect(results).to.equal("Banks, Alex");
+        });
     });
+    
+    describe('loadWiki', function() {
+        // set timeout as 5 seconds to run any asynchronous test under this test suite(the all test folder), default is 2 seconds
+        this.timeout(5000);
+        
+        it("Load Abraham Lincoln's Wikipedia page", function(done) {
+            tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html) {
+                expect(html).to.be.ok;
+                done();
+            });
+        });
+    })
 });
