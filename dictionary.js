ed7f661b6ee50124647c4cd400330918777a989f
SarahZhao806 <yzhao806@gmail.com>
2016-09-06 10:44:56 -0400

    Express routing and cors module

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,22 @@
 var express = require('express');
-
+var cors = require('cors');
 var app = express();
+
+var skierTerms = [
+  {
+    term: 'Rip',
+    defined: 'To move at a high rate of speed'
+  },
+    {
+        term: 'Huck',
+        defined: 'To throw your body off of something, usually a natural feature like a cliff'
+    },
+    {
+        term: 'Chowder',
+        defined: 'Powder after it has been sufficiently skied'
+    }
+    
+];
 
 app.use(function(req, res, next) {
     console.log(`${req.method} request for '${req.url}'`);
@@ -8,6 +24,15 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static("./public1"));
+
+// add cross origin resource sharing to all api requests. So that any domain can make a request for our dictionary api
+app. use(cors());
+
+
+// app.get function will set up a get route 
+app.get('/dictionary-api', function(req, res) {
+    res.json(skierTerms);
+});
 
 app.listen(3000);
 
