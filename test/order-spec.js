d0eff3e9ce2341d4e2193ad9785e4e9e5ab69cb9
SarahZhao806 <yzhao806@gmail.com>
2016-09-06 17:37:11 -0400

    Substited real data with mock data by using rewire module

---
+++ test/order-spec.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var rewire = require("rewire");
+var order = rewire('../lib-rewire/order');
+
+describe("Ordering Items", function() {
+    
+    beforeEach(function() {
+        this.testData = [
+            {sku: 'AAA', qty: 10},
+            {sku: 'BBB', qty: 0},
+            {sku: 'CCC', qty: 3}
+            
+        ];
+        
+        order.__set__('inventoryData', this.testData);
+    });
+    
+    it("order an item when there are enough in stock", function(done) {
+        
+        order.orderItem("CCC", 3, function() {
+            done();
+        });
+        
+        
+    });
+});
